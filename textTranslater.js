8aeef5965c1e1b3b5a8c46f231b2e755e505f734
anton <antonshreyner@gmail.com>
2015-01-18 22:32:26 +0600

    second

--- textTranslater.js
+++ textTranslater.js
@@ -1,6 +1,4 @@
-var context;
-var time = 0.0;
-var canvas;
+var trans_arr = new Array();
 function vec2( x , y )
 {
 	this.x = x;
@@ -17,59 +15,78 @@ function vec2( x , y )
 window_size = new vec2( 256 , 256 );
 function init()
 {
-	canvas = document.getElementById( "canvas" );
-	context = canvas.getContext( "2d" );
 	//window.setInterval( draw , 16 );
-	//alert( translate( "hoch im norden" , "ge" , "ru" ) );
+	//alert( translate( "produced" , "auto" , "ru" ) );
 	printTranslate( document.getElementById( "text" ).textContent );
-}
-function updateSize()
-{
-	window_size = new vec2( canvas.width , canvas.height );
-}
-function clear()
-{
-	context.fillStyle = "#FFFFFF";
-	context.fillRect( 0 , 0 , window_size.x , window_size.y );
 }
 function translate( text , sourceLang , targetLang )
 {
+	var send = text.replace( new RegExp( '[.," ]' , "g" ) , "" );
+	//return send;
 	var url = "https://translate.googleapis.com/translate_a/t?client=t&sl=" 
-            + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI( text );
+            + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI( send );
 	xhr = new XMLHttpRequest();
 	xhr.open( "GET" , url , false );
   	xhr.send( null );
-   return xhr.responseText.replace( new RegExp( "]" , "g" ) , "" )
-   .replace( new RegExp( '\\[' , "g" ) , "" )
-   .split( "," )[0]
-   .replace( new RegExp( '"' , "g" ) , "" );
+    var word_arr = xhr.responseText.replace( new RegExp( "]" , "g" ) , "" )
+    .replace( new RegExp( '\\[' , "g" ) , "" )
+    .replace( new RegExp( '"' , "g" ) , "" )
+    .split( "," );
+    //for( var i = 0; i < word_arr.length; i++ )
+      //  word_arr[i].replace( new RegExp( '"' , "g" ) , "" );
+    return word_arr;
 }
-function drawText( text , color , size , pos )
-{
-	context.fillStyle = color;
-	context.font = size + "pt serif";
-	context.fillText( text , pos.x , pos.y );
-}
-function drawTextCentered( text , color , size , pos )
-{
-	context.fillStyle = color;
-	context.font = size + "pt serif";
-	var text_width = context.measureText( text ).width;
-	context.fillText( text , pos.x - text_width / 2 , pos.y );
-}
-function drawTranslatedTextAsync( text , color , size , pos )
+function printTranslatedTextAsync( text , id , w , i )
 {
 	function afunc()
 	{
-		var transl = translate( text , "auto" , "ru" );
-		drawTextCentered( transl , color , size , pos );
+		/*
+        word_arr[index] = transl;
+		printTextCentered( transl[0] , color , size , pos );*/
+        var transl = translate( text , "ge" , "ru" );
+        trans_arr[i] = transl;
+        var elem = $( "#" + id );
+        elem.append( transl[0] );
+        var lof = elem.offset();
+        var lw = elem.width();
+        elem.offset( { top: lof.top , left: lof.left + w / 2 - lw / 2 } );
 	}
 	setTimeout( afunc , 0 );
 }
 function printTranslate( text )
 {
-	var lines = text.split( "\n" );
-	
+    var lines = text.split( "\n" );
+    var output = $( "#output" );
+    var size = 14;
+    var space = 25;
+    var fsize = 6;
+    var indx = 0;
+    for( var i = 0; i < lines.length; i++ )
+    {
+        if( lines[i] == "" ) continue;
+        var words = lines[i].split( " " );
+        var pos = 0;
+        var ypos = i * space;
+        for( var j = 0; j < words.length; j++ )
+        {
+            if( words[j] == "" ) continue;
+            var id = "word" + i + "" + j;
+            output.append( "<div class='word' id='" + id + 
+                          "' style='left:" + pos + "px;" + " top:" + ypos + "px;" + "'>" + words[j] + "</div>" );
+            var w = $( "#" + id ).width();
+            var lid = "trword" + ( ypos - fsize ) + "" + pos;
+            output.append(
+                "<div class='trword' id='" + lid +
+                "' style='left:" + ( pos ) + "px;" + " top:" + ( ypos - fsize ) + "px;" + "'>" +
+                "</div>"
+            );
+            printTranslatedTextAsync( words[j] , lid , w , indx );
+            pos += w + size;
+            indx++;
+        }
+        output.append( "</br>" );
+    }
+	/*var lines = text.split( "\n" );
 	canvas.width = 512;
 	updateSize();
 	clear();
@@ -77,6 +94,14 @@ function printTranslate( text )
 	var space = 30;
 	canvas.height = lines.length * space;
 	var src_lang = "ge";
+	var width = canvas.width;
+	context.font = size + "pt serif";
+	for( var i = 0; i < lines.length; i++ )
+	{
+		width = Math.max( width , context.measureText( lines[i] ).width );
+	}
+	canvas.width = width;
+    var word_arr = new Array();
 	for( var i = 0; i < lines.length; i++ )
 	{
 		var words = lines[i].split( " " );
@@ -89,5 +114,5 @@ function printTranslate( text )
 			drawTranslatedTextAsync( words[j] , "#008844" , size / 2 , new vec2( pos + word_width / 2 , space * i - size ) );
 			pos += word_width;
 		}
-	}
+	}*/
 }
